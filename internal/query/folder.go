6d959d70aaff0218f92d91a1f05d7233e7ed52a1
alexanderbkl <asederado@gmail.com>
2023-08-27 06:31:42 +0200

    added folder_delete endpoint, updated storage_used on file deleted

--- internal/query/folder.go
+++ internal/query/folder.go
@@ -76,3 +76,51 @@ func FindFolderPathByRoot(root string) entity.Folders {
 
 	return append(FindFolderPathByRoot(m.Root), *m)
 }
+
+
+// FindFolderByUID finds a folder by UID.
+func FindFolderByUID(uid string) (*entity.Folder, error) {
+	m := &entity.Folder{}
+
+	if err := db.Db().Where("uid = ?", uid).First(m).Error; err != nil {
+		return nil, err
+	}
+
+
+	return m, nil
+}
+
+// DeleteFolderByUID deletes a folder by UID.
+func DeleteFolderByUID(uid string) error {
+	if err := db.Db().Where("uid = ?", uid).Delete(&entity.Folder{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+
+// FindFolderUsers finds a user with a certain permission level for a folder
+func FindFolderUser(folderID, userID uint) (*entity.FolderUser, error) {
+	fu := &entity.FolderUser{}
+	if err := db.Db().Where("folder_id = ? AND user_id = ?", folderID, userID).First(fu).Error; err != nil {
+		return nil, err
+	}
+	return fu, nil
+}
+
+
+// GetChildFoldersByUID returns child folders of a given folder.
+func GetChildFoldersByUID(uid string) (folders entity.Folders, err error) {
+	if err := db.Db().Where("root = ?", uid).Find(&folders).Error; err != nil {
+		return folders, err
+	}
+	return folders, nil
+}
+
+
+func GetFolderFilesByUID(folderUID string) (files entity.Files, err error) {
+	if err := db.Db().Where("root = ?", folderUID).Find(&files).Error; err != nil {
+		return files, err
+	}
+	return files, nil
+}
--- internal/query/user_detail.go
+++ internal/query/user_detail.go
@@ -18,3 +18,18 @@ func FindUserDetailByUserID(user_id uint) *entity.UserDetail {
 
 	return m
 }
+
+func FindUserDetailByUserUID(user_uid string) *entity.UserDetail {
+	m := &entity.UserDetail{}
+	stmt := db.Db()
+
+	stmt = stmt.Joins("LEFT JOIN users on users.id = user_details.user_id")
+	stmt = stmt.Where("users.uid = ?", user_uid)
+
+	// Find matching record.
+	if err := stmt.First(m).Error; err != nil {
+		return nil
+	}
+
+	return m
+}
