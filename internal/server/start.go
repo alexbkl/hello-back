100f39a3ac414b49b413201a53a6d6512113e7fa
eroist <97012368+eroist@users.noreply.github.com>
2023-08-31 01:16:53 -0500

    fix: set multipart limit

--- internal/server/start.go
+++ internal/server/start.go
@@ -25,15 +25,28 @@ func Start(ctx context.Context) {
 
 	// Create new HTTP router engine without standard middleware.
 	router := gin.New()
+	router.MaxMultipartMemory = 640 << 20
 
 	// Register common middleware.
 	router.Use(gin.Recovery(), Logger())
 
 	// cors config
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://staging.joinhello.app", "https://joinhello.app", "https://joinhello.vercel.app"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Cross-Origin-Opener-Policy", "Authorization", "PersonalSignature"},
+		AllowOrigins: []string{
+			"http://localhost:5173",
+			"https://staging.joinhello.app",
+			"https://joinhello.app",
+			"https://joinhello.vercel.app",
+		},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Length",
+			"Content-Type",
+			"Cross-Origin-Opener-Policy",
+			"Authorization",
+			"PersonalSignature",
+		},
 		AllowCredentials: false,
 		AllowOriginFunc: func(origin string) bool {
 			return strings.Contains(origin, "hello-storage.vercel.app")
